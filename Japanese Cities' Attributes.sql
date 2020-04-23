03be8a9908fe2ec1804f84e8e50d9479c2a3337b
Shihab <tomdshihab@gmail.com>
2020-04-23 19:07:59 +0600

    Japanese Cities' Attributes

---
+++ Japanese Cities' Attributes.sql
@@ -0,0 +1,6 @@
+--Problem Name : Japanese Cities' Attributes
+--Problem Link : https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+SELECT * FROM city WHERE countrycode = 'JPN';
+
+
