745c1f1612e5177c2fe8db59817e90defb4babfb
Shihab <tomdshihab@gmail.com>
2020-04-23 19:30:43 +0600

    Weather Observation Station 4

---
+++ Weather Observation Station 4.sql
@@ -0,0 +1,5 @@
+--Problem Name : Weather Observation Station 4
+--Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+
+SELECT (COUNT(city)- COUNT(DISTINCT city)) FROM station;
+
