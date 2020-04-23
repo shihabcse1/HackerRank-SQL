27a57eaf302ebf50d02939b0e32b6608e59496e7
Shihab <tomdshihab@gmail.com>
2020-04-23 19:11:14 +0600

    Japanese Cities' Names

---
+++ Japanese Cities' Names.sql
@@ -0,0 +1,5 @@
+--Problem Name : Japanese Cities' Names
+--Problen Link : https://www.hackerrank.com/challenges/japanese-cities-name/problem?h_r=next-challenge&h_v=zen
+
+SELECT name FROM city WHERE countrycode = 'JPN';
+
