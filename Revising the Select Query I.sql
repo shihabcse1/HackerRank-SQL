0967f9282bd11ebfd4d605a2123cdcaab6515e2e
C-1/2 <tomdshihab@gmail.com>
2020-04-25 12:18:40 +0600

    Revising the Select Query I

---
+++ Revising the Select Query I.sql
@@ -0,0 +1,6 @@
+--Problem Name : Revising the Select Query I
+--Problem Link : https://www.hackerrank.com/challenges/revising-the-select-query/problem
+
+SELECT * FROM city WHERE countrycode = 'USA' AND POPULATION > 100000;
+
+
