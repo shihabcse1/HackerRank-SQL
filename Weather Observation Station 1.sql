269291a79cccbcbcf754e83b35f04894ce72a691
C-1/2 <tomdshihab@gmail.com>
2020-04-25 12:21:56 +0600

    Weather Observation Station 1

--- Weather Observation Station 1.sql
+++ Weather Observation Station 1.sql
@@ -1,5 +1,6 @@
 --Problem Name : Weather Observation Station 1
 --Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-1/problem
 
-SELECT city, state FROM station;
+SELECT city, state 
+FROM station;
 
