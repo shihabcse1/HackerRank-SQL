1cd8486071909800f4a93ed00280e78296445458
C-1/2 <tomdshihab@gmail.com>
2020-04-25 12:21:13 +0600

    Weather Observation Station 3

--- Weather Observation Station 3.sql
+++ Weather Observation Station 3.sql
@@ -1,7 +1,8 @@
 --Problem Name : Weather Observation Station 3
 --Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-3/problem?h_r=next-challenge&h_v=zen
 
-SELECT DISTINCT city FROM station 
+SELECT DISTINCT city 
+FROM station 
 WHERE MOD(id, 2) = 0;
 
 
