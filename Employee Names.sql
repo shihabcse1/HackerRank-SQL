6d87453c18673bcaa09366e65c899c6f1fe06930
C-1/2 <tomdshihab@gmail.com>
2020-04-27 12:39:17 +0600

    Employee Names

---
+++ Employee Names.sql
@@ -0,0 +1,14 @@
+--Problem Name : Employee Names
+--Problem Link : https://www.hackerrank.com/challenges/name-of-employees/problem?h_r=next-challenge&h_v=zen
+
+SELECT name
+FROM employee
+ORDER BY name ASC;
+
+-------------------------------- Explanation --------------------------------
+
+--SELECT name ==> Query names 
+--FROM student ==> From employee table 
+--ORDER BY name ASC ==> Order output name in Ascending order.
+
+------------------------------------------------------------------------------
