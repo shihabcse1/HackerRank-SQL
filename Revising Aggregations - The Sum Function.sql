f6fc235654185078d749e43d5cf1ad8956bdae99
C-1/2 <tomdshihab@gmail.com>
2020-04-28 13:13:21 +0600

    Revising Aggregations - The Sum Function

---
+++ Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,9 @@
+--Problem Name : Revising Aggregations - The Sum Function
+--Problem Link : https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?h_r=next-challenge&h_v=zen
+
+SELECT SUM(population)
+FROM city 
+WHERE district = 'california';
+
+
+
