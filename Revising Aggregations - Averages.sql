75369a64aead5a5da73c19685acdab8360ae3ef6
C-1/2 <tomdshihab@gmail.com>
2020-04-28 13:14:29 +0600

    Revising Aggregations - Averages

---
+++ Revising Aggregations - Averages.sql
@@ -0,0 +1,9 @@
+--Problem Name : Revising Aggregations - Averages
+--Problem Link : https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+SELECT AVG(population)
+FROM city 
+WHERE district = 'california';
+
+
+
