bc81fa8eb2cda2c01d1b42ea60c57a0642cdd4c2
C-1/2 <tomdshihab@gmail.com>
2020-04-28 13:13:57 +0600

    Revising Aggregations - The Count Function

---
+++ Revising Aggregations - The Count Function.sql
@@ -0,0 +1,7 @@
+--Problem Name : Revising Aggregations - The Count Function
+--Problem Link : https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+
+SELECT COUNT(id)
+FROM city 
+WHERE population > 100000;
+
