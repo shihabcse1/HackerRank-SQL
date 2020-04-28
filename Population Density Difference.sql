7c38f8a3c4c0cde89499f40e7eacf78f54f91d2e
C-1/2 <tomdshihab@gmail.com>
2020-04-28 13:15:07 +0600

    Population Density Difference

---
+++ Population Density Difference.sql
@@ -0,0 +1,9 @@
+--Problem Name : Population Density Difference
+--Problem Link : https://www.hackerrank.com/challenges/population-density-difference/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+SELECT MAX(population) - MIN(population)
+FROM city;
+
+
+
+
