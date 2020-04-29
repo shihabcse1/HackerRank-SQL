562d91409b5c8b41c51a967cf95bcc7f92f3a357
C-1/2 <tomdshihab@gmail.com>
2020-04-29 11:49:13 +0600

    Japan Population

---
+++ Japan Population.sql
@@ -0,0 +1,8 @@
+--Problem Name : Average Population
+--Problem Link : https://www.hackerrank.com/challenges/japan-population/problem?h_r=next-challenge&h_v=zen
+
+SELECT SUM(population)
+FROM city
+WHERE countrycode = 'jpn';
+
+
